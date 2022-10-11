b5791fced5259b532dee6ee2a302b544ef1fd293
Maximo M VincenteMejia <maximovincente@gmail.com>
2022-10-10 20:02:27 -0400

    added created ditto pokemon to seed.js file for testing

--- seed.js
+++ seed.js
@@ -11,5 +11,76 @@ const {Pokemon} = require('./models/Profile.js');
 async function seed() {
     await Pokemon.create({
         
+            Name: "ditto",
+            Moves: [
+                {
+                    "move": {
+                        "name": "transform",
+                        "url": "https://pokeapi.co/api/v2/move/144/"
+                    },
+                }
+            ],
+          Types:  [
+                {
+                    "slot": 1,
+                    "type": {
+                        "name": "normal",
+                        "url": "https://pokeapi.co/api/v2/type/1/"
+                    }
+                }
+            ],
+          Base_stat:  [
+                {
+                    "base_stat": 48,
+                    "effort": 1,
+                    "stat": {
+                        "name": "hp",
+                        "url": "https://pokeapi.co/api/v2/stat/1/"
+                    }
+                },
+                {
+                    "base_stat": 48,
+                    "effort": 0,
+                    "stat": {
+                        "name": "attack",
+                        "url": "https://pokeapi.co/api/v2/stat/2/"
+                    }
+                },
+                {
+                    "base_stat": 48,
+                    "effort": 0,
+                    "stat": {
+                        "name": "defense",
+                        "url": "https://pokeapi.co/api/v2/stat/3/"
+                    }
+                },
+                {
+                    "base_stat": 48,
+                    "effort": 0,
+                    "stat": {
+                        "name": "special-attack",
+                        "url": "https://pokeapi.co/api/v2/stat/4/"
+                    }
+                },
+                {
+                    "base_stat": 48,
+                    "effort": 0,
+                    "stat": {
+                        "name": "special-defense",
+                        "url": "https://pokeapi.co/api/v2/stat/5/"
+                    }
+                },
+                {
+                    "base_stat": 48,
+                    "effort": 0,
+                    "stat": {
+                        "name": "speed",
+                        "url": "https://pokeapi.co/api/v2/stat/6/"
+                    }
+                }
+            ]
+        
     });
 }
+
+seed();

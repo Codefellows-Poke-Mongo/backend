5512787d45369e7b833c60196ab1a9a4396d4e33
Ben Lieberman <60364696+bhlieberman@users.noreply.github.com>
2022-10-11 20:43:06 -0600

    fixed token issues

--- auth.js
+++ auth.js
@@ -6,30 +6,10 @@ const jwksClient = require('jwks-rsa'); // auth
 // When a user is validated, request.user will contain their information
 // Otherwise, this will force an error
 
-async function verifyUser(request, response, next) {
-
-    function valid(err, user) {
-        request.user = user;
-        next();
-    }
-    const client = jwksClient({
-        // this url comes from your app on the auth0 dashboard
-        jwksUri: process.env.JWKS_URI,
-    });
-
-    const kid = process.env.AUTH0_CLIENT_SECRET;
-    const key = await client.getSigningKey(kid);
-    const signingKey = key.getPublicKey();
-
-    try {
-        const token = request.headers.authorization.split(' ')[1];
-        jwt.verify(token, signingKey, {}, valid);
-    } catch (error) {
-        next('Not Authorized');
-    }
-}
-
-
+const client = jwksClient({
+    // this url comes from your app on the auth0 dashboard
+    jwksUri: process.env.JWKS_URI,
+});
 // =============== HELPER METHODS, pulled from the jsonwebtoken documentation =================== //
 //                 https://www.npmjs.com/package/jsonwebtoken                                     //
 
@@ -37,12 +17,34 @@ async function verifyUser(request, response, next) {
 
 
 // Match the JWT's key to your Auth0 Account Key so we can validate it
-// function getKey(header, callback) {
-//   client.getSigningKey(header.kid, function (err, key) {
-//     const signingKey = key.publicKey || key.rsaPublicKey;
-//     callback(null, signingKey);
-//   });
-// }
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+
+function verifyUser(request, response, next) {
+
+    function valid(err, user) {
+        request.user = user;
+        next();
+    }
+
+
+    // const kid = process.env.AUTH0_CLIENT_SECRET;
+    // const key = await client.getSigningKey(kid);
+    // const signingKey = key.getPublicKey();
+
+    try {
+        const token = request.headers.authorization.split(' ')[1];
+        jwt.verify(token, getKey, {}, valid);
+    } catch (error) {
+        next('Not Authorized');
+    }
+}
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict'
 
 require('dotenv').config();
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const express = require('express');
 const PokemonPath = require('./modules/pokemon-paths.js');
@@ -10,14 +9,10 @@ const verifyUser = require('./auth.js')
 const mongoose = require('mongoose');
 
 const app = express();
-app.use(cors());
 
 //middleware
+app.use(cors());
 app.use(express.json());
-// var authenticate = jwt({
-//     secret: process.env.AUTH0_CLIENT_SECRET,
-//     audience: process.env.AUTH0_CLIENT_ID
-//   });
 app.use(verifyUser);
 
 mongoose.connect(process.env.DB_URL);
@@ -26,9 +21,6 @@ db.on('error', console.error.bind(console, 'mongodb connection error'));
 db.once('open', function () {
     console.log('Mongoose is connected');
 });
-
-const Profile = require('./models/Profile.js');
-
 
 //Endpoints
 app.get('/', (request, response) => {
